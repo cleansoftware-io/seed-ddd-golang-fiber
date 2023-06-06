b140e273197944ff54361b26507080a064e1ef19
Tirso Garcia <tibanez@topdoctors.es>
2023-06-06 13:32:31 +0200

    remove the comments on the most basic test example

--- tests/is_even_test.go
+++ tests/is_even_test.go
@@ -6,19 +6,19 @@ import (
 )
 
 func TestIsEven(t *testing.T) {
-	// Caso de prueba 1: Número par
+
 	result := IsEven(4)
-	assert.True(t, result, "4 debería ser un número par")
+	assert.True(t, result, "4 must be an even number")
 
-	// Caso de prueba 2: Número impar
+
 	result = IsEven(7)
-	assert.False(t, result, "7 no debería ser un número par")
+	assert.False(t, result, "7 must not be an even number")
 
-	// Caso de prueba 3: Número negativo par
+
 	result = IsEven(-10)
-	assert.True(t, result, "-10 debería ser un número par")
+	assert.True(t, result, "-10 must be an even number")
 
-	// Caso de prueba 4: Número negativo impar
+
 	result = IsEven(-13)
-	assert.False(t, result, "-13 no debería ser un número par")
+	assert.False(t, result, "-13 must not be an even number)"
 }
