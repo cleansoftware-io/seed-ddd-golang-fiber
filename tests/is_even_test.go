c67c0f66548093c1cf2624537f5fa23331e2564c
Tirso Garcia <tibanez@topdoctors.es>
2023-06-06 13:33:26 +0200

    fix the mistake

--- tests/is_even_test.go
+++ tests/is_even_test.go
@@ -10,15 +10,12 @@ func TestIsEven(t *testing.T) {
 	result := IsEven(4)
 	assert.True(t, result, "4 must be an even number")
 
-
 	result = IsEven(7)
 	assert.False(t, result, "7 must not be an even number")
-
 
 	result = IsEven(-10)
 	assert.True(t, result, "-10 must be an even number")
 
-
 	result = IsEven(-13)
-	assert.False(t, result, "-13 must not be an even number)"
+	assert.False(t, result, "-13 must not be an even number")
 }
