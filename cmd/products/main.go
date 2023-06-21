c39910c8b5358e7cf5d91dffc5e5c58855ac9c97
Tirso Garcia <tgarciaib@gmail.com>
2023-06-21 15:15:49 +0200

    I prefer a DI container to manage all dependencies to avoid complex constructors and complex app initialization.

--- cmd/products/main.go
+++ cmd/products/main.go
@@ -10,9 +10,15 @@ import "github.con/tgarcia/seed-ddd-golang-fiber/internal/application"
 func Run(bootstrap *initialization.Bootstrap) {
 
 	logger := adapters.NewLoggerImpl(bootstrap.Logger)
-	productsService := adapters.NewDeactivateProductsIml(logger)
 
+	productsService := adapters.NewDeactivateProductsIml(logger)
 	deactivateProductUseCase := application.NewDeactivateProductUseCase(logger, productsService)
+
+	// ... more use cases
+	// ... more services
+	// ... more controllers
+	// This method is a little ugly, I prefer a DI container to manage all dependencies
+	// to avoid complex constructors and complex app initialization.
 
 	productsControllers := controllers.NewProducts(bootstrap.App, bootstrap.Logger, deactivateProductUseCase)
 	productsControllers.RegisterRoutes()
